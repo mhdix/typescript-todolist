58fb0e036adc5089b4106872975f6ce11262f539
mhdix <domanloomu@gmail.com>
2025-03-19 14:12:18 -0300

    feature: complate typeScript user list

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+import './style.css'
+
+type Item = {
+    id: string,
+    name: string,
+    email: string,
+}
+
+
+const form = document.querySelector<HTMLFormElement>('#add-user-form')
+
+const name = document.querySelector<HTMLInputElement>('#name')
+const email = document.querySelector<HTMLInputElement>('#email')
+
+const users = document.querySelector<HTMLDivElement>("#user-list")
+
+form?.addEventListener('submit', e => {
+    e.preventDefault()
+
+    if (name?.value == undefined || email?.value === undefined) return
+
+    const userItem: Item = {
+        id: uuidv4(),
+        name: name.value,
+        email: email.value,
+    }
+    localStorage.setItem('user', JSON.stringify(userItem))
+    addUser(userItem)
+    name.value = ''
+    email.value = ''
+
+
+})
+function addUser(item: Item) {
+    const container = document.createElement('div')
+
+    const nameElement = document.createElement('p')
+    const emailElement = document.createElement('p')
+
+    nameElement.append(item.name)
+    emailElement.append(item.email)
+
+    container.append(nameElement, emailElement)
+
+    users?.append(container)
+}
---
+++ src/todo.ts
@@ -0,0 +1,11 @@
+export class todo {
+    static currentId = 0;
+
+    id: number;
+    task: string;
+    complated: boolean;
+
+    constructor(task: string) {
+        this.id = todo.
+    }
+}
